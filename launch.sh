d5e291086d4c2da46ea2fc53c205506d1ef2b504
Hans Donner <hans@hansdonner.nl>
2014-10-17 19:22:19 +0200

    allow acl

--- launch.sh
+++ launch.sh
@@ -6,9 +6,16 @@ SQUID_MAX_OBJ_SIZE=${SQUID_MAX_OBJ_SIZE:-1024}
 SQUID_MAX_MEM_OBJ_SIZE=${SQUID_MAX_MEM_OBJ_SIZE:-50}
 SQUID_MAX_CACHE_SIZE=${SQUID_MAX_CACHE_SIZE:-5000}
 
-SQUID_PARENT_HOST=${:-}
-SQUID_PARENT_PORT=${:-3128}
-SQUID_PARENT_PORT_UDP=${:-0}
+SQUID_PARENT_HOST=${SQUID_PARENT_HOST:-}
+SQUID_PARENT_PORT=${SQUID_PARENT_PORT:-3128}
+SQUID_PARENT_PORT_UDP=${SQUID_PARENT_PORT_UDP:-0}
+
+SQUID_ACL=${SQUID_ACL:-}
+SQUID_ALWAYS_DIRECT=${SQUID_ALWAYS_DIRECT:-}
+SQUID_NEVER_DIRECT=${SQUID_NEVER_DIRECT:-}
+if [ -z "${SQUID_NEVER_DIRECT} ${SQUID_ALWAYS_DIRECT" ]; then
+  SQUID_NEVER_DIRECT="allow all"
+fi
 
 if [ -n "${SQUID_PARENT_HOST}" ]; then
   SQUID_PARENT_ENABLED=${SQUID_PARENT_ENABLED:-true}
@@ -19,13 +26,30 @@ SQUID_PARENT_ENABLED=${SQUID_PARENT_ENABLED:-false}
 chown -R proxy:proxy /var/cache/squid3
 
 # change config
+# - cache size
 echo "maximum_object_size ${SQUID_MAX_OBJ_SIZE} MB" >> $CONF
 echo "maximum_object_size_in_memory ${SQUID_MAX_MEM_OBJ_SIZE} MB" >> $CONF
 echo "cache_dir ufs /var/cache/squid3 ${SQUID_MAX_CACHE_SIZE} 16 256" >> $CONF
 
+# - upstream proxy
 if [ "${SQUID_PARENT_ENABLED}" == "true" ]; then
   echo "cache_peer ${SQUID_PARENT_HOST} parent ${SQUID_PARENT_PORT} ${SQUID_PARENT_PORT_UDP} no-query default" >> $CONF
-  echo "never_direct allow all" >> $CONF
+  
+  IFS=";" read -ra ACL <<< "$SQUID_ACL"
+  for i in "${ACL[@]}"; do
+    echo "acl ${i}" >> $CONF
+  done
+
+  IFS=";" read -ra DIRECT <<< "$SQUID_ALWAYS_DIRECT"
+  for i in "${DIRECT[@]}"; do
+    echo "always_direct ${i}" >> $CONF
+  done
+
+  IFS=";" read -ra DIRECT <<< "$SQUID_NEVER_DIRECT"
+  for i in "${DIRECT[@]}"; do
+    echo "never_direct ${i}" >> $CONF
+  done
+
 fi
 
 # start
